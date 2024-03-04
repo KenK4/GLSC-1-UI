a01c0d063d480daf826c019822b0cd09d463149d
KenK4 <byakuchiya@gmail.com>
2024-03-04 19:56:57 +0700

    doneeeeeeeee

--- Assets/Scripts/Check.cs
+++ Assets/Scripts/Check.cs
@@ -7,9 +7,32 @@ public class Check : MonoBehaviour
 {
     public TMP_InputField email;
     public TMP_InputField password;
-
+    public TMP_Text em;
+    public TMP_Text pw;
     public void Validation()
     {
+        if (email.text == "binus@binus.edu")
+        {
+            em.text = "";
+
+        }
+        else
+        {
+            em.text = "*email is invalid";
+            em.color = Color.red;
+        }
+
+        if (password.text == "12345")
+        {
+            pw.text = "";
+            //UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            pw.text = "*incorrect password";
+            pw.color = Color.red;
+        }
+
         if (email.text == "binus@binus.edu" && password.text == "12345")
         {
             UnityEditor.EditorApplication.isPlaying = false; 
